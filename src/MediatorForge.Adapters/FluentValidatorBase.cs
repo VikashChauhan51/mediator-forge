effb1e0babb0c0e1866ff134709b294e25e9cec1
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-12-24 22:03:39 +0530

    added fluent validator

---
+++ src/MediatorForge.Adapters/FluentValidatorBase.cs
@@ -0,0 +1,27 @@
+﻿using MediatorForge.CQRS.Validators;
+using MediatorForge.Utilities;
+
+namespace MediatorForge.Adapters;
+public class FluentValidatorBase<TRequest> : FluentValidation.AbstractValidator<TRequest>, IValidator<TRequest>
+{
+    new public async Task<ValidationResult> ValidateAsync(TRequest request, CancellationToken cancellationToken = default)
+    {
+        var validationResult = await base.ValidateAsync(request, cancellationToken);
+
+        if (validationResult.IsValid)
+        {
+            return ValidationResult.Success;
+        }
+
+        var errors = validationResult
+            .Errors
+            .Select(e =>
+            new ValidationError
+            (
+                e.PropertyName,
+                e.ErrorMessage,
+                e.AttemptedValue
+            ));
+        return ValidationResult.Failure(errors);
+    }
+}
